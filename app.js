3760c6b5ed6b4f303365395fe23a879b5d1a9894
mahammadshariaralamsarker <154776315+mahammadshariaralamsarker@users.noreply.github.com>
2024-02-28 14:20:26 +0600

    4th

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const loadPhone = async (searchText) => {
     const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
     const data = await res.json();
     const phones = data.data;
-    console.log(phones);
+    // console.log(phones);
     displayPhones(phones);
 }
 const displayPhones = (phones) => {
@@ -17,7 +17,7 @@ const displayPhones = (phones) => {
         showallcontainer.classList.add('hidden');
     }
     phones = phones.slice(0, 12);
-    console.log(phones.length);
+    // console.log(phones.length);
 
     // phones = phones.slice(0,5);
     phones.forEach(phone => {
@@ -30,8 +30,8 @@ const displayPhones = (phones) => {
                     <div class="card-body">
                         <h2 class="card-title">${phone.phone_name}</h2>
                         <p>If a dog chews shoes whose shoes does he choose?</p>
-                        <div class="card-actions justify-end">
-                            <button class="btn btn-primary">Buy Now</button>
+                        <div class="card-actions justify-center">
+                            <button onclick="showModal('${phone.slug}');show_my_modal_details.showModal()" class="btn btn-primary">Show Details</button>
                         </div>
                     </div>
         
@@ -49,17 +49,25 @@ const handleSearch = () => {
     // console.log(searchText);
     loadPhone(searchText);
 }
-const loadingcontainer= (isloading)=> {
-    const loadingcontainers =document.getElementById('loading-container');
-    if(isloading){
+const loadingcontainer = (isloading) => {
+    const loadingcontainers = document.getElementById('loading-container');
+    if (isloading) {
         loadingcontainers.classList.remove('hidden');
     }
     else {
         loadingcontainers.classList.add('hidden');
     }
 }
-
-
-
-
+const showModal = async (id) => {
+    // console.log('click',id);
+    const res = await fetch(`https://openapi.programming-hero.com/api/phone/apple_iphone_13_pro_max-${id}`);
+    const data = await res.json();
+    // console.log(data);
+}
+const showModalDetails = (phone) => {
+    console.log(phone);
+    show_my_modal_details.showModal(phone);
+    const phoneName = document.getElementById('show-details-phone-name');
+    phoneName.innerText = phone.brand;
+}
 loadPhone();
